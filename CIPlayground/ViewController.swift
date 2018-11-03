8af622b5413a149c1551fb8a72b35ca0d98a24ff
Vlad Averin <hello@vladaverin.me>
2018-11-03 14:05:37 -0400

    Project init

---
+++ CIPlayground/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CIPlayground
+//
+//  Created by Vladislav Averin on 2018-11-03.
+//  Copyright © 2018 Orbitum Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
